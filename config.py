5cda26ea6e395d19506dcd51eb46770839b60d92
Etaminchik <43892972+Etaminchik@users.noreply.github.com>
2022-12-15 11:55:04 +0300

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+FROM = "e.tolkachuk@spectredpi.com"
+TO = ["etaminmail@yandex.ru","e.tolkachuk@spectredpi.com"]
+PASSWORD = ''
+
+
+#Костыльная транслитерация без библиотек)))
+symbols = (u"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+           u"abvgdeejzijklmnoprstufhzcss_y_euaABVGDEEJZIJKLMNOPRSTUFHZCSS_Y_EUA")
