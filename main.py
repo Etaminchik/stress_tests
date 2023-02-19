77f4ba9f012199f259e740c191bdf4c5141f0f30
Etaminchik <43892972+Etaminchik@users.noreply.github.com>
2023-02-19 21:53:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-from config import *
 from email.utils import COMMASPACE, formatdate
 
 
