6dc89fb9563b1ac88ce9dd99871ca51dc8509ef1
Etaminchik <43892972+Etaminchik@users.noreply.github.com>
2023-02-19 20:27:06 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-FROM = "e.tolkachuk@spectredpi.com"
-TO = ["etaminmail@yandex.ru","e.tolkachuk@spectredpi.com"]
-PASSWORD = ''
-
-
-#Костыльная транслитерация без библиотек)))
-symbols = (u"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
-           u"abvgdeejzijklmnoprstufhzcss_y_euaABVGDEEJZIJKLMNOPRSTUFHZCSS_Y_EUA")
